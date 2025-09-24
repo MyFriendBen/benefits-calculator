0d352baa0052838e5b9af740b1b14a35f79202ce
Katie Brey <katie@codethedream.org>
2025-09-24 11:46:09 -0400

    leave utm params in url

--- src/Components/CampaignAnalytics/useCampaign.tsx
+++ src/Components/CampaignAnalytics/useCampaign.tsx
@@ -1,29 +1,13 @@
 import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import { setCampaign } from './campaign';
-import useUtmParameters, { UtmKeys } from './useUtmParameters';
-
-const useRemoveUtmParams = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  return () => {
-    const params = new URLSearchParams(location.search);
-    UtmKeys.forEach((param) => params.delete(param));
-
-    const newUrl = `${location.pathname}${params.toString() ? `?${params}` : ''}`;
-    navigate(newUrl, { replace: true });
-  };
-};
+import useUtmParameters from './useUtmParameters';
 
 // Pulls the utm parameters, saves, and removes from the URL
 export default function useCampaign() {
   const utmParameters = useUtmParameters();
-  const removeUtmParams = useRemoveUtmParams();
   useEffect(() => {
     if (utmParameters) {
       setCampaign(utmParameters);
-      removeUtmParams();
     }
-  }, [utmParameters, removeUtmParams]);
+  }, [utmParameters]);
 }
