6e57043bab07a6be6c53fc52ddee1a19f03be4fd
ming <ming@codethedream.org>
2025-10-14 15:52:07 -0700

    use acuteCondition text to display need text

--- src/Components/Results/NcLink211Message.tsx
+++ src/Components/Results/NcLink211Message.tsx
@@ -1,90 +1,78 @@
 import { FormattedMessage } from 'react-intl';
-import { useContext } from 'react';
+import { ReactNode, useContext } from 'react';
 import { Context } from '../../Components/Wrapper/Wrapper';
+import { useConfig } from '../Config/configHook';
+import { FormattedMessageType } from '../../Types/Questions';
 
 type Topics = {
-  id: string,
   topic: string;
   subtopic: string;
   keyword: string;
-  displayText: string;
+};
+
+type IconAndFormattedMessageMap = {
+  [key: string]: {
+    text: FormattedMessageType;
+    icon: ReactNode;
+  };
 };
 
 export default function NcLink211Message() {
   const NEEDS_MAPPING: Record<string, Topics> = {
     food: {
-      id: 'link211nc.food',
       topic: 'Basic%20Needs',
       subtopic: 'Food',
       keyword: 'Food groceries',
-      displayText: 'Food or groceries',
     },
     housing: {
-      id: 'link211nc.housing',
       topic: 'Basic%20Needs',
       subtopic: 'Housing%2FShelter',
       keyword: 'rent utilities mortgage',
-      displayText:'Help with managing your mortgage, rent, or utilities',
     },
     childDevelopment: {
-      id: 'link211nc.childDevelopment',
       topic: 'Education%2CIndividual%20and%20Family%20Life',
       subtopic: 'Educational%20Institutions%2FSchools%2CEducational%20Programs%2CEducational%20Support%20Services%2CIndividual%20and%20Family%20Support%20Services%2CSocial%20Development%20and%20Enrichment',
       keyword: 'child development',
-      displayText: "Concern about your child's development",
     },
     familyPlanning: {
-      id: 'link211nc.familyPlanning',
       topic: 'Health%20Care',
       subtopic: 'Human%20Reproduction',
       keyword: 'Family planning',
-      displayText: 'Family planning or birth control',
     },
     support: {
-      id: 'link211nc.support',
       topic: 'Mental+Health+and+Substance+Use+Disorder+Services',
       subtopic: 'Mental+Health+Assessment+and+Treatment%2CMental+Health+Support+Services',
       keyword: 'mental health',
-      displayText: 'A challenge you or your child would like to talk about',
     },
     dentalCare: {
-      id: 'link211nc.dentalCare',
       topic: 'Health+Care',
       subtopic: 'Specialized+Treatment+and+Prevention%2CSpecialty+Medicine',
       keyword: 'dental care',
-      displayText: 'Low-cost dental care',
     },
     jobResources: {
-      id: 'link211nc.jobResources',
       topic: 'Income%20Support%20and%20Employment',
       subtopic: 'Employment%2CTemporary%20Financial%20Assistance',
       keyword: 'Job resources',
-      displayText: 'Job resources',
     },
     legalServices: {
-      id: 'link211nc.legalServices',
-      topic: 'Criminal%20Justice%20and%20Legal%20Services', //Criminal+Justice+and+Legal+Services
-      subtopic: 'Legal%20Services', //Legal+Services
+      topic: 'Criminal%20Justice%20and%20Legal%20Services',
+      subtopic: 'Legal%20Services',
       keyword: 'civil legal services',
-      displayText: 'Free or low-cost help with civil legal needs or identity documents',
     },
     babySupplies: {
-      id: 'link211nc.babySupplies',
       topic: 'Material%20Goods',
       subtopic: 'Basic%20Needs',
       keyword: 'baby supplies',
-      displayText: 'Diapers and other baby supplies',
     },
     veteranServices: {
-      id: 'link211nc.verteranServices',
       topic: 'Organizational%2FCommunity%2FInternational+Services%2CTarget+Populations',
       subtopic: 'Military+Service%2CMilitary+Personnel%2FContractors',
       keyword: 'veterans services',
-      displayText: 'Veterans services',
     },
   };
 
   const { formData } = useContext(Context);
+  const acuteConditionOptions = useConfig<IconAndFormattedMessageMap>('acute_condition_options');
   const zipcode = formData.zipcode;
 
   const needsData = formData.acuteHHConditions;
@@ -124,8 +112,8 @@ export default function NcLink211Message() {
   return(
     <div> 
       <FormattedMessage id="link211nc.resources" defaultMessage="More local resources from NC211: " />
-      {Object.entries(householdNeeds).map(([key, value], index, arr) => (
-        <div key={key} style={{display: "inline-block", fontWeight: '700', fontFamily: 'Open Sans, sans-serif', marginBottom: "0.5rem"}}>
+      {Object.entries(householdNeeds).map(([key, value]) => (
+        <div key={key} style={{display: "inline-block", fontWeight: '700', fontFamily: 'Open Sans, sans-serif', marginBottom: "0.5rem", marginRight: "0.5rem"}}>
           <a
             href={buildQueryURL(value)}
             target="_blank"
@@ -138,11 +126,8 @@ export default function NcLink211Message() {
               color: "#264D91",
             }}
           >
-            {/* {value.displayText} */}
-            <FormattedMessage id={value.id} defaultMessage={value.displayText} />
+            {acuteConditionOptions[key].text}
           </a>
-          {/* Add separator only between items */}
-          {index < arr.length - 1 && <span> | </span>}
         </div>
       ))}
     </div>
