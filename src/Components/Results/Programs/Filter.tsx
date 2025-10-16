4ab0b45816f362066689b8d050875e02149896b8
IrynaKolh <iryna.kolhanova@gmail.com>
2025-10-16 14:03:59 -0400

    update styles after reviev

--- src/Components/Results/Programs/Filter.tsx
+++ src/Components/Results/Programs/Filter.tsx
@@ -255,7 +255,13 @@ export const Filter = () => {
         <FormattedMessage id="filterSection.header" defaultMessage="Filter Results by Citizenship" />
       </h2>
       <div className="filter-description">
-        <Typography variant="body2" color="text.secondary" sx={{ margin: '0.5rem 0 1rem 0', fontSize: '0.9rem', fontFamily: 'Roboto, Arial, sans-serif', fontWeight: 400 }}>
+        <Typography variant="body2" color="text.secondary" sx={{
+          fontFamily: "'Open Sans', serif",
+          fontSize: '1rem',
+          fontWeight: 200,
+          color: 'black',
+          marginBottom: '1rem',
+        }}>
           <FormattedMessage
             id="filterSection.citizenHelpText"
             defaultMessage="Household members may have mixed immigration status. This means that some people in your home may qualify for benefits even if others do not. Use this filter to see how a person's status affects their ability to qualify. If you sign up for a public benefit or file a tax return, your family’s information may be shared with immigration officials."
