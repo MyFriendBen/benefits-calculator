e37d3e34b797cd69fedada4f55e10a11f4f10369
Josh Mejia <josh@myfriendben.org>
2025-11-06 16:09:19 -0700

    wip - remove immediate needs click tracking

--- src/Components/OptionCardGroup/MultiSelectTiles.tsx
+++ src/Components/OptionCardGroup/MultiSelectTiles.tsx
@@ -5,7 +5,6 @@ import { ReactComponent as Checkmark } from '../../Assets/icons/General/OptionCa
 import './MultiSelectTiles.css';
 import { useIntl } from 'react-intl';
 import { Context } from '../Wrapper/Wrapper';
-import dataLayerPush from '../../Assets/analytics';
 
 export type MultiSelectTileOption<T extends string | number> = {
   value: T;
@@ -72,17 +71,7 @@ function MultiSelectTiles<T extends string | number>({ options, values, onChange
       {options.map((option, index) => {
         const onClick = () => {
           let newValues: Record<T, boolean> = { ...values };
-          const isNowSelected = !newValues[option.value];
-          newValues[option.value] = isNowSelected;
-
-          // Track tile click event
-          dataLayerPush({
-            event: 'tile_click',
-            category: 'immediate_needs',
-            action: 'select_tile',
-            label: String(option.value),
-            selected: isNowSelected,
-          });
+          newValues[option.value] = !newValues[option.value];
 
           onChange(newValues);
         };
