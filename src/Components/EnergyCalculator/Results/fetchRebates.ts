4e9cd594390107b492ee70372a14560ae6c6a328
Caton Hauer <caton@myfriendben.org>
2025-11-18 10:18:22 -0700

    Revert "rebate changes"
    This reverts commit d9e7526715180420600ca5a412d651c746f3d29e.

--- src/Components/EnergyCalculator/Results/fetchRebates.ts
+++ src/Components/EnergyCalculator/Results/fetchRebates.ts
@@ -82,10 +82,7 @@ function createQueryString(formData: FormData, lang: Language) {
 
 async function getRebates(formData: FormData, lang: Language) {
   const queryString = createQueryString(formData, lang);
-  const apiUrl = `https://api.rewiringamerica.org/api/v1/calculator${queryString}`;
-  console.log('Fetching rebates from:', apiUrl);
-
-  const res = await fetch(apiUrl, {
+  const res = await fetch(`https://api.rewiringamerica.org/api/v1/calculator${queryString}`, {
     method: 'GET',
     headers: {
       Authorization: API_KEY,
@@ -94,9 +91,6 @@ async function getRebates(formData: FormData, lang: Language) {
 
   const data = (await res.json()) as EnergyCalculatorAPIResponse;
 
-  console.log('API Response - Total incentives:', data.incentives.length);
-  console.log('All incentive programs:', data.incentives.map(i => i.program));
-
   const rebateCategories: EnergyCalculatorRebateCategory[] = [];
 
   for (const rebate of data.incentives) {
@@ -104,6 +98,10 @@ async function getRebates(formData: FormData, lang: Language) {
 
     for (const item of rebate.items) {
       const category = ENERGY_CALCULATOR_CATEGORY_MAP[item];
+
+      if (category === undefined) {
+        continue;
+      }
 
       categories.add(category);
     }
