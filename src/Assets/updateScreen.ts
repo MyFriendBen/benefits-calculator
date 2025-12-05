6def7fe28758c8841188aa7676cea2dc1cbff1a9
SonaliBedge <sonali@codethedream.org>
2025-12-05 09:25:25 -0800

    Add NC - HeadStart Program.

--- src/Assets/updateScreen.ts
+++ src/Assets/updateScreen.ts
@@ -80,6 +80,7 @@ const getScreensBody = (formData: FormData, languageCode: Language, whiteLabel: 
     has_nc_lieap: formData.benefits.nc_lieap ?? null,
     has_ncwap: formData.benefits.ncwap ?? null,
     has_nccip: formData.benefits.nccip ?? null,
+    has_nc_head_start: formData.benefits.nc_head_start ?? null,
     has_csfp: formData.benefits.csfp ?? null,
     has_ccdf: formData.benefits.ccdf ?? null,
     has_aca: formData.benefits.aca ?? null,
