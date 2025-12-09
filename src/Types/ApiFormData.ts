6b1a062b2c17915744c208215adcb27c193cbbd2
Pat Wey <pat@myfriendben.org>
2025-12-09 15:25:50 -0800

    Add MA: Early Head Start (#2002)
    - Add early_head_start field to form data and update related functions
    
    https://linear.app/myfriendben/issue/MFB-270/ma-add-head-startearly-head-start

--- src/Types/ApiFormData.ts
+++ src/Types/ApiFormData.ts
@@ -201,6 +201,7 @@ export type ApiFormData = {
   has_ma_maeitc: boolean | null;
   has_ma_macfc: boolean | null;
   has_head_start: boolean | null;
+  has_early_head_start: boolean | null;
   has_co_andso: boolean | null;
   has_co_care: boolean | null;
   has_project_cope: boolean | null;
