3d1d99ad265ef79536f546e7fed131502fca111a
Pat Wey <pat@myfriendben.org>
2025-12-17 14:22:16 -0800

    Add MA: DHSP
    Support ma_dhsp_afterschool field
        https://linear.app/myfriendben/issue/MFB-528/ma-add-dhsp-afterschool-programs-lottery-k-8

--- src/Assets/updateFormData.tsx
+++ src/Assets/updateFormData.tsx
@@ -76,6 +76,7 @@ export function useUpdateFormData() {
         ma_maeitc: response.has_ma_maeitc ?? false,
         ma_macfc: response.has_ma_macfc ?? false,
         ma_homebridge: response.has_ma_homebridge ?? false,
+        ma_dhsp_afterschool: response.has_ma_dhsp_afterschool ?? false,
         head_start: response.has_head_start ?? false,
         early_head_start: response.has_early_head_start ?? false,
         co_andso: response.has_co_andso ?? false,
