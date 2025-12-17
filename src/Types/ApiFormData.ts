7cc2e5ea793e0f050526263b3be62cc48ad4a89d
Pat Wey <pat@myfriendben.org>
2025-12-17 12:13:54 -0800

    Add MA: HomeBridge (#2017)
    https://linear.app/myfriendben/issue/MFB-526/ma-add-homebridge

--- src/Types/ApiFormData.ts
+++ src/Types/ApiFormData.ts
@@ -200,6 +200,7 @@ export type ApiFormData = {
   has_ma_mbta: boolean | null;
   has_ma_maeitc: boolean | null;
   has_ma_macfc: boolean | null;
+  has_ma_homebridge: boolean | null;
   has_head_start: boolean | null;
   has_early_head_start: boolean | null;
   has_co_andso: boolean | null;
