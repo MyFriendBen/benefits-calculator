303d4bdf18d8f649700b5eebde3e5c26e75da65d
nihalerdal <nihalerdall@gmail.com>
2026-02-19 10:58:06 -0800

    show 211 prepopulated resources in Additional Resources banner (match general MFB behavior)

--- src/Components/Results/Needs/Needs.tsx
+++ src/Components/Results/Needs/Needs.tsx
@@ -4,6 +4,7 @@ import { useResultsContext, useResultsLink } from '../Results';
 import NeedCard from './NeedCard';
 import { ResultsMessageForNeeds } from '../../Referrer/Referrer';
 import InformationalText from '../../Common/InformationalText/InformationalText';
+import NcLink211Message from '../NcLink211Message';
 
 const Needs = () => {
   const { needs } = useResultsContext();
@@ -22,6 +23,7 @@ const Needs = () => {
   return (
     <div data-testid="needs-section">
       <ResultsMessageForNeeds />
+      <NcLink211Message />
       <InformationalText>
         <FormattedMessage
           id="nearTermBenefits.editSelections"
