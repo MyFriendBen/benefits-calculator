f8b67f8595ff87a080820c4f9d3d2db24f5b312b
nihalerdal <nihalerdall@gmail.com>
2026-07-16 09:36:38 -0700

    remove additional helper steps

--- tests/helpers/steps.ts
+++ tests/helpers/steps.ts
@@ -61,8 +61,6 @@ export async function selectCondition(page: Page, condition: string) {
 }
 
 export async function selectIncome(page: Page, incomeCategory: string, incomeType: string, frequency: string, amount: number) {
-  await page.locator('button.income-add-button').click();
-  await page.locator('#income-stream-0').waitFor({ state: 'visible' });
   await selectIncomeCategory(page, incomeCategory);
   await selectIncomeType(page, incomeType);
   await selectFrequency(page, frequency);
